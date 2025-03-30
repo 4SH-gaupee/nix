958232c6c18c6a7541199dfac846e98cea89634a
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-03-30 16:45:48 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = { 
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.gaupee = import ./home.nix;
+
+          }
+ 
+      ];
+    };
+  };
+};
+}
---
+++ home.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "gaupee";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+    minikube
+    docker-compose
+    ansible
+    btop  # replacement of htop/nmon
+    curl 
+    dnsutils  # `dig` + `nslookup`
+    ethtool
+    fzf # A command-line fuzzy finder
+    gawk
+    gcc
+    git
+    gnupg
+    gnused
+    gnutar
+    iftop # network monitoring
+    iotop # io monitoring
+    jq # A lightweight and flexible command-line JSON processor
+    k9s
+    k9s
+    kdePackages.ksshaskpass
+    kubectl
+    kubeswitch
+    lm_sensors # for `sensors` command
+    lsof # list open files
+    mtr # A network diagnostic tool
+    nix-output-monitor
+    oh-my-zsh
+    pciutils # lspci
+    ripgrep # recursively searches directories for a regex pattern
+    slack
+    sysstat
+    terraform
+    terragrunt
+    tmux
+    tree
+    unzip
+    kubernetes-helm
+    usbutils # lsusb
+    wget
+    which
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    zig
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Gwendal AUPEE";
+    userEmail = "gwendal.aupee@4sh.fr";
+  };
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    history.ignoreDups = true;
+    history.extended = true;
+    shellAliases = {
+      nixedit = "nvim ~/.config/nixos";
+      nixup   = "nixos-rebuild switch --use-remote-sudo --flake ~/.config/nixos";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "docker" "terraform" "ansible" "tmux" "kube-ps1"];
+      theme = "ys";
+    };
+    initExtra = ''
+      PROMPT='$(kube_ps1)'$PROMPT
+    '';
+  };
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+      withNodeJs = true;
+      withPython3 = true;
+      extraPackages = with pkgs; [
+        # LSP Servers
+        clang-tools
+        hadolint
+        helm-ls
+        lua-language-server
+        marksman
+        nil
+        nodePackages.bash-language-server
+        python3Packages.python-lsp-server
+        shellcheck
+
+        terraform-lsp
+        tflint
+        yaml-language-server
+	ansible-language-server
+
+        # Formatters
+        nixfmt-rfc-style
+	hclfmt
+	xmlformat
+        shfmt
+	prettierd
+	];
+  };
+
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
