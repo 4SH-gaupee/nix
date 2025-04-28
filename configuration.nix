002abb07eaffc05636c52dbf3cfb1c01a96837b4
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-04-28 21:01:08 +0200

    style: cleaning

--- configuration.nix
+++ configuration.nix
@@ -67,17 +67,6 @@
 };
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-#  services.openvpn.servers = {
-#    CinVPN  = { config = '' 
-#		  config /home/gaupee/.ssh/cin.ovpn 
-#		  auth-user-pass /home/gaupee/.ssh/cin-vpn-secret
-#	'';
-#     };
-#  };
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -163,31 +152,12 @@
     displaylink
   ];
   programs.ssh.startAgent = true;
-
+  programs.kdeconnect.enable = true;
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
   virtualisation.docker.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- flake.nix
+++ flake.nix
@@ -6,16 +6,12 @@
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = { 
+    nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
