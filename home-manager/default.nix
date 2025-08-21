7118a8e9ccb8c0eadb80c03f43ebbafc7ff1d169
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-08-21 12:01:22 +0200

    chore: Update flake & remove hyprland config to avoid unused code

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -2,45 +2,29 @@
 {
   imports = [
    ../modules/home-manager/default.nix
-    #   inputs.walker.homeManagerModules.default
   ];
   home.username = "gaupee";
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     (python313.withPackages (ps: [ ps.hvac ps.diagrams ]))
-    minikube
-    qbittorrent
-    foot
-    gh
-    ghq
-    kubectx
-    mpv
-    networkmanager-openvpn
-    qpwgraph
-    vault
-    trivy
-    bitwarden-cli
-    buildpack
-    prusa-slicer
-    govc
-    mongodb-compass
-    kustomize
-    docker-compose
     ansible
     btop  # replacement of htop/nmon
     curl
     dnsutils  # `dig` + `nslookup`
-    popeye
+    docker-compose
     ethtool
+    foot
     fzf # A command-line fuzzy finder
-    talosctl
     gawk
     gcc
+    gh
+    ghq
     git
     gnupg
     gnused
     gnutar
+    govc
     iftop # network monitoring
     iotop # io monitoring
     jq # A lightweight and flexible command-line JSON processor
@@ -48,31 +32,38 @@
     kdePackages.ksshaskpass
     kdePackages.xdg-desktop-portal-kde
     kubectl
+    kubectx
+    kubernetes-helm
     kubeswitch
+    kustomize
     lm_sensors # for `sensors` command
     lsof # list open files
+    minikube
+    mongodb-compass
+    mpv
     mtr # A network diagnostic tool
+    networkmanager-openvpn
     nix-output-monitor
     oh-my-zsh
     pciutils # lspci
+    popeye
+    qpwgraph
     ripgrep # recursively searches directories for a regex pattern
     slack
     sysstat
+    talosctl
     terraform
     terragrunt
     tmux
     tree
+    trivy
     unzip
-    kubernetes-helm
     usbutils # lsusb
+    vault
     wget
     which
     yq-go # yaml processor https://github.com/mikefarah/yq
     zig
-    # Hyprland
-    waybar
-    dunst
-    walker
  ];
 
   # basic configuration of git, please change to your own
@@ -108,63 +99,6 @@
       source <(switcher init zsh)
       source <(switch completion zsh)
     '';
-  };
-
-
-  ###   HYPRLAND
-
-  #  programs.walker = {
-  #  enable = true;
-  #  runAsService = true;
-  #
-  #  # All options from the config.json can be used here.
-  #  config = {
-  #    search.placeholder = "Example";
-  #    ui.fullscreen = true;
-  #    list = {
-  #      height = 200;
-  #    };
-  #    websearch.prefix = "?";
-  #    switcher.prefix = "/";
-  #  };
-  #
-  #  };
-
-  programs.kitty.enable = true; # required for the default Hyprland config
-  wayland.windowManager.hyprland.enable = true; # enable Hyprland
-  home.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  wayland.windowManager.hyprland.extraConfig = ''
-    exec-once = nm-applet --indicator
-  '';
-
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    input = {
-      kb_layout = "fr";
-    };
-
-    bind =
-      [
-        "$mod, F, exec, firefox"
-	"$mod, T, exec, kitty"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-	      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-	      "$mod, R, exec, walker"
-	      "$mod, M, exec, wlogout --protocol layer-shell"
-	      "$mod, Q, killactive,"
-            ]
-          )
-          9)
-      );
   };
 
   # This value determines the home Manager release that your
