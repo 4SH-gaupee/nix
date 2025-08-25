7532b7215e81039af6dd79d4156e5938fd491543
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-08-25 11:32:15 +0200

    Neovim: Add options

--- modules/home-manager/cli/neovim/files/options.lua
+++ modules/home-manager/cli/neovim/files/options.lua
@@ -5,7 +5,35 @@ vim.opt_local.tabstop = 2 -- Number of spaces that a <Tab> in the file counts fo
 vim.opt_local.expandtab = true -- Expand tab to 2 spaces
 vim.opt.fileformats = "unix,dos"
 vim.opt.fileformat = "unix"
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.breakindent = true
+-- Save undo history
+vim.opt.undofile = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+-- Decrease update time
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+-- Configure how new splits should be opened
+vim.opt.splitright = false
+vim.opt.splitbelow = false
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 8
 -- disable lsplog
 -- This is not usefull on a daily basis and should positively impact performance
 vim.lsp.set_log_level("off")
