00698074aa29ef55b6a3e5fe0e11c0ed8139ea8c
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-12-15 17:16:02 +0100

    chore(boot): Reduce EFI partition load by reducing number of generation keept

--- configuration.nix
+++ configuration.nix
@@ -60,6 +60,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   networking.hostName = "nixos"; # Define your hostname.
   networking.hosts = {
