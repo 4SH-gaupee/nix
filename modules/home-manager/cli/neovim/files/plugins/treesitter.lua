d616d67c3331f275b9447ac2a1ceaf2ac3054584
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2026-01-20 15:25:23 +0100

    fix(NVIM): Fix updated treesitter api

--- modules/home-manager/cli/neovim/files/plugins/treesitter.lua
+++ modules/home-manager/cli/neovim/files/plugins/treesitter.lua
@@ -1,9 +1,8 @@
-local ts = require 'nvim-treesitter.configs'
+local ts = require("nvim-treesitter.config")
 
-ts.setup {
-  sync_install = false,
-  highlight = {
-    enable = true,
-  }
-}
-
+ts.setup({
+	sync_install = false,
+	highlight = {
+		enable = true,
+	},
+})
